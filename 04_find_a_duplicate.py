0cd831f3bf31dec7dfec8ab65dfc2160f6cbdb0e
varshitha8142 <69466185+varshitha8142@users.noreply.github.com>
2021-01-29 22:22:39 +0530

    Done

---
+++ 04_find_a_duplicate.py
@@ -0,0 +1,41 @@
+""" Find a duplicate
+
+we are given a list of integers where:
+
+the integers are in the range 1..n
+the list has a length of n+1
+These properties mean the list must have at least 1 duplicate. 
+
+Our challenge was to find a duplicate number, while optimizing for space. 
+We can use a divide and conquer approach, iteratively cutting the list in half 
+to find a duplicate integer in O(n log n) time and O(1) space 
+(sort of a modified binary search).
+
+But we can actually do better. We can find a duplicate integer in O(n) time 
+while keeping our space cost at O(1).
+
+This is a tricky one to derive (unless you have a strong background in graph theory)
+
+Imagine each item in the list as a node in a linked list. 
+In any linked list, each node has a value and a "next" pointer. In this case:
+
+The value is the integer from the list.
+The "next" pointer points to the value-eth node in the list (numbered starting 
+from 1). 
+
+For example, if our value was 3, the "next" node would be the third node.
+
+Here’s a full example:
+
+A list [2, 3, 1, 3], so 2 is in the first position and points to 3 in the second 
+position. Notice we're using "positions" and not "indices." 
+
+For this problem, we'll use the word "position" to mean something like "index," 
+but different: indices start at 0, while positions start at 1. 
+
+More rigorously: position = index + 1.
+
+Using this, find a duplicate integer in O(n) time while keeping our space cost at 
+O(1).  """
+
+# Start coding from here
---
+++ 09_reverse_linked_list.py
@@ -0,0 +1,13 @@
+""" Write a function for reversing a linked list.
+Your function will have one input: the head of the list.
+Your function should return the new head of the list.
+
+Here's a sample linked list node class:
+
+  class LinkedListNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next  = None   """
+
+# Start coding from here
