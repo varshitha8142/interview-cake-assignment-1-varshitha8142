e79bda5c932e2304976909e07c233b5b94907991
varshitha8142 <69466185+varshitha8142@users.noreply.github.com>
2021-01-29 22:37:51 +0530

    Updated

--- 09_reverse_linked_list.py
+++ 09_reverse_linked_list.py
@@ -11,3 +11,35 @@ Here's a sample linked list node class:
         self.next  = None   """
 
 # Start coding from here
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+head=0
+for i in range(7):
+    if(head==0):
+        head=node(i)
+        n=head
+    else:
+        n.next=node(i)
+        c=n
+        n=n.next
+p=head
+print("Before Reverse linked list:")
+while(p!=None):
+    print(p.data)
+    p=p.next 
+def f(p):
+    global head
+    prev = None
+    while p:
+        n = p.next
+        p.next = prev 
+        prev = p  
+        p = n  
+    head = prev
+f(head) 
+print("Reverse linked list:")
+while(n!=None):
+    print(n.data)
+    n=n.next
